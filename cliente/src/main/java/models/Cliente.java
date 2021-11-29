94aad65d84eeb19c96f78e01ccb783a95e4c2109
Israel Pontes <israel.pontes.1920@gmail.com>
2021-11-29 11:50:41 -0300

    first commit

---
+++ cliente/src/main/java/models/Cliente.java
@@ -0,0 +1,122 @@
+package models;
+
+import java.util.Date;
+
+public class Cliente {
+    
+    private String nome;
+    private String sobrenome;
+    private String idade;
+    private String sexo;
+    private String cpf;
+    private String RG;
+    private String email;
+    private Date dataNascimento;
+
+    /**
+     * @return String return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return String return the sobrenome
+     */
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    /**
+     * @param sobrenome the sobrenome to set
+     */
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    /**
+     * @return String return the idade
+     */
+    public String getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    /**
+     * @return String return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    /**
+     * @return String return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return String return the RG
+     */
+    public String getRG() {
+        return RG;
+    }
+
+    /**
+     * @param RG the RG to set
+     */
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+    /**
+     * @return Date return the dataNascimento
+     */
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    /**
+     * @param dataNascimento the dataNascimento to set
+     */
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+}
